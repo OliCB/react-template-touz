918e5b71d92b28f93800a9ced64b5710f9c55d97
Olivier Couture-Bienvenue <olivier.couture-bienvenue@vooban.com>
2025-01-13 16:46:57 -0500

    Add user filter

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import styles from "./styles/App.module.css";
 import { User } from "./types/User";
 import { Status } from "./types/Status";
+import { useMemo, useState } from "react";
 
 export default function App() {
   const { data } = useQuery<User[]>({
@@ -13,9 +14,35 @@ export default function App() {
     refetchInterval: 5000,
   });
 
+  const [userFilter, setUserFilter] = useState<string | undefined>();
+  const filteredUsers = useMemo(
+    () => (userFilter ? data?.filter((user) => user.id === userFilter) : data),
+    [data, userFilter]
+  );
+  const userIds = useMemo(() => data?.map((user) => user.id), [data]);
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Process tracker</h1>
+      <form style={{ marginBottom: "1rem" }}>
+        <label>User: </label>
+        <select
+          onChange={(e) => {
+            e.stopPropagation();
+            setUserFilter(e.target.value);
+          }}
+          value={userFilter}
+        >
+          {[
+            <option key="filter-default"></option>,
+            ...(userIds?.map((u) => (
+              <option key={`filter-${u}`} value={u}>
+                {u}
+              </option>
+            )) ?? []),
+          ]}
+        </select>
+      </form>
       <table className={styles.table}>
         <thead>
           <tr>
@@ -25,7 +52,7 @@ export default function App() {
           </tr>
         </thead>
         <tbody>
-          {data?.map((user) =>
+          {filteredUsers?.map((user) =>
             user.processes.map((process) => (
               <tr key={`${user.id}-${process.id}`}>
                 <td>{user.id}</td>
